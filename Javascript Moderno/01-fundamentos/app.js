b75720fd4b0406e2ecdd55840457607d61f8be7d
KyotaDev <luiszegarrafernandez@gmail.com>
2024-08-06 16:48:40 -0500

    Initial commit: Add all courses code

---
+++ Javascript Moderno/01-fundamentos/app.js
@@ -0,0 +1,10 @@
+//console.log( console.log('Hola mundo') );
+
+let a = 10, 
+    b = 10, 
+    c = 10, 
+    d = 10, 
+    x = a + b;
+
+console.log(x);
+// c = 20; // No se puede reasignar un valor a una constante
