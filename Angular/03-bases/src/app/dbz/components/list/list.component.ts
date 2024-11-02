6e923a11210e9fc51beed45bfde4473fc213a4e3
KyotaDev <luiszegarrafernandez@gmail.com>
2024-11-02 02:06:39 -0500

    Seccion 5: Expandiendo Bases de Angular

---
+++ Angular/03-bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  @Input()
+  public characterList : Character[] = [{
+    id: '0',
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete : EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id? : string) : void {
+    if(!id) return;
+    console.log({id});
+    this.onDelete.emit(id);
+  }
+}
